e70adfcb89282b60e17a0e8ebd6f5fe2d0066869
mb234567 <marioblackman791@gmail.com>
2022-07-06 15:30:55 -0400

    Commit

--- client/List Tasks/ListTasks.js
+++ client/List Tasks/ListTasks.js
@@ -2,4 +2,13 @@ Template.ListT.helpers({
     theTasks(){
         return ToDodb.find();
     }
+}) 
+
+Template.ListT.events({
+    'click .js-delete5'() {
+        $("#LModal").modal("hide");
+        console.log("nigger")
+    } 
+    
+
 })
